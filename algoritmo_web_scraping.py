400a5bda245e9c3ece6646794acc021003c6db31
Fábio Oliveira <fabio.oliveira1403@outlook.com>
2023-11-22 15:03:48 -0300

    Terminando algoritmo Web Scraping

--- algoritmo_web_scraping.py
+++ algoritmo_web_scraping.py
@@ -2,9 +2,11 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 import re
-todos_dados = [] # variavel pra armazenar todos os dados e consumir toda memorio do meu computador
+
+todos_dados = []  # variavel pra armazenar todos os dados e consumir toda memorio do meu computador
 
 navegador = webdriver.Edge()
+navegador.set_window_size(800, 600)
 
 navegador.get('https://veiculos.fipe.org.br/')
 
@@ -17,7 +19,6 @@ navegador.find_element('xpath', '//*[@id="selectTabelaReferenciacarro"]').click(
 sleep(2)
 
 site = BeautifulSoup(navegador.page_source, 'lxml')
-
 
 anos = site.find(id='selectTabelaReferenciacarro').find_all('option')
 marcas = site.find(id='selectMarcacarro').find_all('option')
@@ -34,6 +35,9 @@ for i in range(1, len(anos) + 1):
         ano_modelo = site.find(id='selectAnoModelocarro').find_all('option')
 
         for k in range(2, len(ano_modelo) + 1):
+            navegador.find_element('xpath', f'//*[@id="selectMarcacarro"]/option[{j}]').click()
+            sleep(1)
+
             navegador.find_element('xpath', f'//*[@id="selectAnoModelocarro"]/option[{k}]').click()
             sleep(1)
             site = BeautifulSoup(navegador.page_source, 'lxml')
@@ -54,15 +58,13 @@ for i in range(1, len(anos) + 1):
                     if m % 2 != 0:
                         dados.append(re.sub('<[^<]+?>', '', str(informacoes[m])))
                 print(dados)
-                dados.append(todos_dados) #Colocando em todos os dados
+                dados.append(todos_dados)  # Colocando em todos os dados
                 sleep(1)
 
+                if l == len(ano_carro):
+                    navegador.find_element('xpath', '//*[@id="buttonLimparPesquisarcarro"]/a').click()
 
     print(i)
 
-
-
-
-#res = soup.find('ul', class_='chosen-results')
-#print(soup)
-
+# res = soup.find('ul', class_='chosen-results')
+# print(soup)
